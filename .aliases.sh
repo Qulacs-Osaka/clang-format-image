71cc555073cf769d608ecd1df6f55dcee413e89b
KowerKoint <kowerkoint@gmail.com>
2022-06-15 14:13:34 +0900

    USE_TEST=Yes in .aliases.sh

--- .aliases.sh
+++ .aliases.sh
@@ -2,8 +2,8 @@ CONTAINER_NAME=ghcr.io/qulacs-osaka/qulacs-ubuntu-pyenv:latest
 CONTAINER_NAME_FORMAT=ghcr.io/qulacs-osaka/qulacs-ubuntu-clang-format:latest
 
 # Use single quote to avoid variable expansion.
-RUN_CONTAINER='docker run --rm -it -v `pwd`:/qulacs-osaka'
-RUN_CONTAINER_UNPRIVILEGED='docker run --rm -it -v `pwd`:/qulacs-osaka -e LOCAL_UID=`id -u $USER` -e LOCAL_GID=`id -g $USER`'
+RUN_CONTAINER='docker run --rm -it -v `pwd`:/qulacs-osaka -e USE_TEST=Yes'
+RUN_CONTAINER_UNPRIVILEGED='docker run --rm -it -v `pwd`:/qulacs-osaka -e LOCAL_UID=`id -u $USER` -e LOCAL_GID=`id -g $USER` -e USE_TEST=Yes'
 
 alias qulacs_run="$RUN_CONTAINER_UNPRIVILEGED $CONTAINER_NAME"
 alias qulacs_test="$RUN_CONTAINER_UNPRIVILEGED $CONTAINER_NAME make test -C build -j"

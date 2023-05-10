e42bc771479f7ca22089224dfe45ddef1f3f55a7
Koki Kawamura <kowerkoint@gmail.com>
2023-05-10 10:50:45 +0900

    update clang-format target

--- qulacs_format.sh
+++ qulacs_format.sh
@@ -5,4 +5,4 @@ PROJECT=/qulacs
 # create .clang-format if not exists
 [ -f $PROJECT/.clang-format ] || cp /.clang-format $PROJECT
 
-find $PROJECT/src $PROJECT/test $PROJECT/benchmark -regex '.*\.\(cu\|cuh\|cpp\|h\|hpp\)' -exec clang-format -style=file -i {} \;
+find $PROJECT/src $PROJECT/test $PROJECT/benchmark $PROJECT/python -regex '.*\.\(cu\|cuh\|cpp\|h\|hpp\)' -exec clang-format -style=file -i {} \;

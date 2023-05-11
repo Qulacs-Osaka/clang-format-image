a78ae6fa7cd94d7ad7b354b3f8bb8b46f0549368
kowerkoint <kowerkoint@gmail.com>
2023-05-11 16:35:00 +0900

    update qulacs_format.sh, generate_stub.sh

--- generate_stub.sh
+++ generate_stub.sh
@@ -3,10 +3,4 @@
 PROJECT=/qulacs
 cd $PROJECT
 
-stubgen -p qulacs_osaka_core -o typings
-pybind11-stubgen qulacs_osaka_core --no-setup-py --root-module-suffix="" --ignore-invalid=all --output-dir="./typings"
-
-stubgen -p qulacs_osaka -o typings
-pybind11-stubgen qulacs_osaka --no-setup-py --root-module-suffix="" --ignore-invalid=all --output-dir="./typings"
-
-cd -
+./update_stubs.sh
